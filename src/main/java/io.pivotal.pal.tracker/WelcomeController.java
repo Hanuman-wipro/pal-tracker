40d2671daa8a6eebf22a6fc40ca7a51030d78d85
Hanuman-wipro <hanuman.pandey@wipro.com>
2019-07-02 11:21:08 -0700

    Lab 2

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -1,16 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class WelcomeController {
+public class WelcomeController {
 
+    private final String hello;
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+        return hello;
     }
 
 
+    public WelcomeController(@Value("${welcome.message}") String message ){
+        this.hello=message;
+    }
 }

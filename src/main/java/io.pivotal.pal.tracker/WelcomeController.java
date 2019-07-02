fff6bb39bc7ca447426c37aedf76ca1a5b315013
hanuman-wipro <hanuman-wipro>
2019-07-01 18:10:01 -0700

    Lab1 submisson

---
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -0,0 +1,16 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class WelcomeController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "hello";
+    }
+
+
+}
